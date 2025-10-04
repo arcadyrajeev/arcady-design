03b602009ffd5b98a35adde0cd5cc59a28969153
arcadyrajeev <bitmasteratj@gmail.com>
2025-10-04 21:42:47 +0530

    new sfa

--- src/components/inBetween.tsx
+++ src/components/inBetween.tsx
@@ -7,7 +7,7 @@ interface sectionProps {
 const InBetween: React.FC<sectionProps> = ({ text }) => {
   return (
     <div>
-      <h1 className="fontheading2 flex text-[2rem] text-secondary w-full py-20 justify-center">
+      <h1 className="fontheading2 flex text-[2rem] text-secondary p-10 w-full py-20 justify-center">
         {text}
       </h1>
     </div>
